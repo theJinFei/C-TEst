e2e26578e7df245f1c1f35a74fd8c02966690f94
luojinfei <luojinfei_study@163.com>
2017-12-28 10:14:05 +0800

    fix some bugs

--- gftest2/main.cpp
+++ gftest2/main.cpp
@@ -2,7 +2,15 @@
 #include <iomanip>
 #include <cmath>
 #include <stdio.h>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
+
+void print(int (*x)[3], int row_size); //打印出传给的矩阵
+int fun_addAndSubtract(int a, int b); //有限域内加法和减法（都是基于异或的）
+int fun_Multiply(int a, int b); //有限域内的乘法（基于查表的）
+int fun_divide(int a, int b); //有限域内的除法（基于反查表的）
+int fun_GaussBaseGf(int (*a)[3], int row_size); //实现高斯消元 求解矩阵的逆
 
 //想实现 将伽罗华域映射为柯西矩阵
 //最后寻找最优的柯西矩阵使总的运算量 最小
@@ -28,6 +36,27 @@ const vector<vector<int> > multi_v = {
     {0, 7, 5, 2, 1, 6, 4, 3}
 };
 
+
+
+
+int main()
+{
+    int a[3][3] = {{0, 1, 0},
+              {6, 4, 3},
+            {7, 5, 3}};
+    fun_GaussBaseGf(a, 3);
+    return 0;
+}
+//打印出矩阵
+void print(int (*x)[3], int row_size)
+{
+    for(int i = 0; i < row_size; i++){
+        for(int j = 0; j < 3; j++){
+            cout << setw(10) << x[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 //分别对应伽罗华域中的运算 加减--异或 乘为查表操作 除以反查表操作
 int fun_addAndSubtract(int a, int b)
 {
@@ -39,37 +68,25 @@ int fun_Multiply(int a, int b)
 }
 int fun_divide(int a, int b)
 {
-    for(int k = 0; k < 7; k++){
+    for(int k = 0; k < 8; k++){
         if(multi_v[b][k] == a){
             return k;
         }
     }
 }
 
-void print(double (*x)[3], int row_size)
+//高斯消元法
+int fun_GaussBaseGf(int (*a)[3], int row_size)
 {
-    for(int i = 0; i < row_size; i++){
-        for(int j = 0; j < 3; j++){
-            cout << setw(10) << x[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-int main()
-{
-    int N = 3;
-    double a[3][3] = {{0, 1, 2},
-                {-2, 2, 0},
-                {3, 1, 3}};
-    double b[3][3] = {{1, 0, 0},
-                {0, 1, 0},
-                {0, 0, 1}};
     cout << "原始矩阵为：" << endl;
     print(a, 3);
-    int i, j, k;
+    int b[3][3] = {{1, 0, 0},
+              {0, 1, 0},
+            {0, 0, 1}}; //最后得出的逆矩阵
+    int i, j, k, N = 3;
     //查找主元
     for(i = 0; i < N; i++){
-        double max = a[i][i];
+        int max = a[i][i];
         k = i;
         for(j = i + 1; j < N; j++){
             if(abs(a[j][i]) > abs(max)){
@@ -80,7 +97,7 @@ int main()
         //如果第i行不是主元，则将第i行与第k行进行互换(记得要连着单位矩阵)
         if(k != i){
             for(j = 0; j < N; j++){
-                double temp = a[i][j];
+                int temp = a[i][j];
                 a[i][j] = a[k][j];
                 a[k][j] = temp;
 
@@ -95,21 +112,21 @@ int main()
         //判断主元是否为0，若是，则矩阵A不是满秩矩阵
         if(a[i][i] == 0){
             cout << "There is no inverse matrix!" << endl;
-            return 0
+            return 0;
         }
 
-        double temp = a[i][i]; //temp为要收缩的比例
+        int temp = a[i][i]; //temp为要收缩的比例
         for(k = 0; k < N; k++){
-            a[i][k] = a[i][k] / temp;
-            b[i][k] = b[i][k] / temp;
+            a[i][k] = fun_divide(a[i][k], temp);
+            b[i][k] = fun_divide(b[i][k], temp);
         }
 
         for(j = 0; j < N; j++){
             if(j != i){
-                double temp = a[j][i];
+                int temp = a[j][i];
                 for(k = 0; k < N; k++){
-                    a[j][k] = a[j][k] - a[i][k] * temp;
-                    b[j][k] = b[j][k] - b[i][k] * temp;
+                    a[j][k] = fun_addAndSubtract(a[j][k], fun_Multiply(a[i][k], temp));
+                    b[j][k] = fun_addAndSubtract(b[j][k], fun_Multiply(b[i][k], temp));
                 }
             }
         }
@@ -119,15 +136,5 @@ int main()
     }
     cout <<"最后变换后矩阵为：" << endl;
     print(b, N);
-
-
-
-
-
-
-
-
-
-
-
+    return 1;
 }
